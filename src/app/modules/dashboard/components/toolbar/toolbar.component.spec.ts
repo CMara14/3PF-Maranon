71dcaf730362c4675b8e3cb1d480a941ff7443cb
Your Name <constanza.aymara@gmail.com>
2025-02-19 23:23:03 -0300

    fix test

--- src/app/modules/dashboard/components/toolbar/toolbar.component.spec.ts
+++ src/app/modules/dashboard/components/toolbar/toolbar.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ToolbarComponent } from './toolbar.component';
+import { SharedModule } from '../../../../shared/shared.module';
 
 describe('ToolbarComponent', () => {
   let component: ToolbarComponent;
   let fixture: ComponentFixture<ToolbarComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ToolbarComponent]
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ToolbarComponent],
+      imports: [SharedModule],
     })
     .compileComponents();
 
@@ -17,7 +19,9 @@ describe('ToolbarComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should instantiate the component', () => {
+    const fixture = TestBed.createComponent(ToolbarComponent);
+    expect(fixture.componentInstance).toBeTruthy();
   });
+
 });
